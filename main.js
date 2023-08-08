7c08001fce18ebe77b75472591016c7a5432e4a1
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-08 20:31:01 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,10 @@ const refreshBtn = document.querySelector(".refresh-btn");
 const generatePalette = () => {
 container.innerHTML = ""; 
 }
+
+
+// container
+ for (let i = 0; i < PaletteBoxes; i++) {
+
+ }
+ 
