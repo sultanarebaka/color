b279f4e98cd9379fc1f1a3afda0df26fceb06019
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-16 20:04:56 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,13 @@
 
-
 const container = document.querySelector(".container");
 const refreshBtn = document.querySelector(".refresh-btn");
 
-refreshBtn.addEventListener("click", generatePalette);
-        
-        // the container
-        const color = document.createElement("li");
-        
-        color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
-                           <span class="hex-value">${randomHex}</span>`;
-      
-      const generatePalette = () => {
-    container.innerHTML = ""; //  the container
+const maxPaletteBoxes = 32;
+
+const generatePalette = () => {
+    container.innerHTML = ""; // clearing the container
     for (let i = 0; i < maxPaletteBoxes; i++) {
-      
-      
-        // hex code
-        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
-        randomHex = `#${randomHex.padStart(6, "0")}`;
         
-      
-        //copy color
-        color.addEventListener("click", () => copyColor(color, randomHex));
-        container.appendChild(color);
-    }
-}
-
-
-
-
-generatePalette();
-
-const copyColor = (elem, hexVal) =>
-
-
-setTimeout(() => colorElement.innerText = hexVal, 1000);
-    });
-
-colorElement.innerText = "Copied"; 
-
-
-
-
-color.addEventListener("click", () => copyColor(color, randomHex));
-        container.appendChild(color);
-    
-
-
-
-const copyColor = (elem, hexVal) => {
-    const colorElement = elem.querySelector(".hex-value");
-    }
-
-
-
-
-
 
 
 
