1d08663bb1b1a129765ac75ddf9dc7ee3b5f8868
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-21 16:49:42 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -8,16 +8,16 @@ const maxPaletteBoxes = 32;
 const generatePalette = () => {
     container.innerHTML = ""; // clearing the container
     for (let i = 0; i < maxPaletteBoxes; i++) {
-        // generating a random hex color code
+        // hex color code
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
         randomHex = `#${randomHex.padStart(6, "0")}`;
         
-        // creating a new 'li' element and inserting it to the container
+        // the container
         const color = document.createElement("li");
         color.classList.add("color");
         color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                            <span class="hex-value">${randomHex}</span>`;
-        // adding click event to current li element to copy the color
+        // copy the color
         color.addEventListener("click", () => copyColor(color, randomHex));
         container.appendChild(color);
     }
@@ -26,8 +26,9 @@ generatePalette();
 
 const copyColor = (elem, hexVal) => {
     const colorElement = elem.querySelector(".hex-value");
-    // Copying the hex value, updating the text to copied, 
-    // and changing text back to original hex value after 1 second
+
+
+    
     navigator.clipboard.writeText(hexVal).then(() => {
         colorElement.innerText = "Copied";
         setTimeout(() => colorElement.innerText = hexVal, 1000);
@@ -35,5 +36,4 @@ const copyColor = (elem, hexVal) => {
 }
 
 refreshBtn.addEventListener("click", generatePalette);
-
 
