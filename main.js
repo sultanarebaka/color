000a2ca6b28b4d342462dc77e1118aec8696868e
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-08-21 15:26:46 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const container = document.querySelector(".container");
 const refreshBtn = document.querySelector(".refresh-btn");
 
+        
+        // creating a new 'li
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
+                           <span class="hex-value">${randomHex}</span>`;
+
+
+
 const maxPaletteBoxes = 32;
 
 const generatePalette = () => {
@@ -11,12 +20,7 @@ const generatePalette = () => {
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
         randomHex = #${randomHex.padStart(6, "0")};
         
-        // creating a new 'li
-        const color = document.createElement("li");
-        color.classList.add("color");
-        color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
-                           <span class="hex-value">${randomHex}</span>`;
-        // adding click event to current li element to copy the color
+        // copy the color
         color.addEventListener("click", () => copyColor(color, randomHex));
         container.appendChild(color);
     }
